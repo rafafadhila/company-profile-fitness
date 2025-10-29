aa33f6f992075faeba0884a55a272315b694b3de
rafafadhila <rafafadhila03@gmail.com>
2025-10-30 00:31:03 +0700

    services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+
+export default function page() {
+  return (
+    <main>
+      <Header
+        image="/images/header.jpg"
+        title="We Provides What You Need"
+        subtitle="Join us and feel the difference in every move.">
+      </Header>
+    </main>
+  )
+}
