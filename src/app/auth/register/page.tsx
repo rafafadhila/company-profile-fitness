389d3e19751aa6b200db24f30609f43dbc2cac0d
rafafadhila <rafafadhila03@gmail.com>
2025-11-01 10:51:41 +0700

    create register page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+
+export default function page() {
+    return (
+        <main className='bg-white h-screen'>
+            <div className='h-screen flex justify-center items-center text-black p-3'>
+                <div className='w-sm bg-gray-100 p-5 shadow-md'>
+                    <form className='flex flex-col text-left gap-5' >
+                        <h2 className='text-2xl font-bold text-center'>Create an Account</h2>
+                        <input
+                            type="text"
+                            placeholder='Full Name'
+                            className=' bg-white py-3 px-4 rounded-md'
+                            name='name'
+                        />
+                        <input
+                            type="text"
+                            placeholder='Email Address'
+                            className=' bg-white py-3 px-4 rounded-md'
+                            name='email'
+                        />
+
+                        <input
+                            type="password"
+                            placeholder='Password'
+                            className='bg-white py-3 px-4 rounded-md'
+                            name='password'
+                        />
+
+                        <button
+                            type="submit"
+                            className='bg-purple-700 text-white py-3 rounded-md font-bold'>Register</button>
+
+                            <p>Have an account? <Link href={'/auth/login'} className='font-semibold text-blue-600'>Login</Link></p>
+                    </form>
+
+                </div>
+            </div>
+        </main>
+    )
+}
