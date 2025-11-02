e6d1850d14a0f7942732f069e0967b382d78ad8c
rafafadhila <rafafadhila03@gmail.com>
2025-11-02 22:49:37 +0700

    backendless app_id and api_key

---
+++ src/utils/backendless.ts
@@ -0,0 +1,8 @@
+import Backendless from 'backendless';
+
+const APP_ID = '6311E72C-1336-4A4B-A878-9CDE248B1685';
+const API_KEY = 'A33C980F-C001-49A7-8A40-5DEAF3F5734D';
+
+Backendless.initApp(APP_ID, API_KEY);
+
+export default Backendless;
