10fe7b286cb13d92a52452d17c04ac5c6508d97b
rafafadhila <rafafadhila03@gmail.com>
2025-11-03 15:17:07 +0700

    add email and logout in navbar if user logged in

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import useAuthStore from "@/stores/useAuthStore";
 
 export default function Navbar() {
+
+  const { user, logout } = useAuthStore();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -21,11 +24,31 @@ export default function Navbar() {
         </ul>
 
         {/* Sign In Button (Desktop Only) */}
-        <Link 
-        href={'/auth/login'}
-        className="hidden md:block bg-purple-700 hover:bg-purple-900 px-5 py-2 rounded-full font-bold text-md transition">
-          LOGIN
-        </Link>
+        <div className="hidden md:flex">
+          {!user?.objectId ? (
+            <>
+              <Link
+                href="/auth/login"
+                className="bg-purple-700 hover:bg-purple-900 px-5 py-2 rounded-full font-bold text-md transition">
+                Login
+              </Link>
+            </>
+          ) : (
+            <>
+              <span className="bg-purple-600 p-2 m-2 rounded-md font-bold text-sm transition">
+                {user?.email}
+              </span>
+              <button
+                onClick={logout}
+                className="text-sm text-red-600 hover:underline"
+              >
+                Logout
+              </button>
+            </>
+          )}
+
+        </div>
+
 
         {/* Hamburger Menu Button (Mobile) */}
         <button
@@ -34,28 +57,24 @@ export default function Navbar() {
           aria-label="Toggle Menu"
         >
           <span
-            className={`block h-0.5 bg-white transform transition duration-300 ${
-              isOpen ? "rotate-45 translate-y-2" : ""
-            }`}
+            className={`block h-0.5 bg-white transform transition duration-300 ${isOpen ? "rotate-45 translate-y-2" : ""
+              }`}
           ></span>
           <span
-            className={`block h-0.5 bg-white transition duration-300 ${
-              isOpen ? "opacity-0" : ""
-            }`}
+            className={`block h-0.5 bg-white transition duration-300 ${isOpen ? "opacity-0" : ""
+              }`}
           ></span>
           <span
-            className={`block h-0.5 bg-white transform transition duration-300 ${
-              isOpen ? "-rotate-45 -translate-y-2" : ""
-            }`}
+            className={`block h-0.5 bg-white transform transition duration-300 ${isOpen ? "-rotate-45 -translate-y-2" : ""
+              }`}
           ></span>
         </button>
       </nav>
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden bg-indigo-900 text-white border-t border-indigo-800 transition-all duration-300 overflow-hidden ${
-          isOpen ? "max-h-96 py-6" : "max-h-0 py-0"
-        }`}
+        className={`md:hidden bg-indigo-900 text-white border-t border-indigo-800 transition-all duration-300 overflow-hidden ${isOpen ? "max-h-96 py-6" : "max-h-0 py-0"
+          }`}
       >
         <ul className="flex flex-col items-center gap-5 font-semibold text-lg">
           <li><a href="/" className="hover:text-indigo-400 transition" onClick={() => setIsOpen(false)}>Home</a></li>
@@ -64,9 +83,27 @@ export default function Navbar() {
           <li><a href="/services" className="hover:text-indigo-400 transition" onClick={() => setIsOpen(false)}>Services</a></li>
           <li><a href="/blog" className="hover:text-indigo-400 transition" onClick={() => setIsOpen(false)}>Blog</a></li>
           <li>
-            <button className="bg-purple-700 hover:bg-purple-900 px-5 py-2 rounded-full font-bold text-md transition">
-              SIGN IN
-            </button>
+            {!user?.objectId ? (
+              <>
+                <Link
+                  href="/auth/login"
+                  className="bg-purple-700 hover:bg-purple-900 px-5 py-2 rounded-full font-bold text-md transition">
+                  Login
+                </Link>
+              </>
+            ) : (
+              <>
+                <span className="bg-purple-600 p-2 m-2 rounded-md font-bold text-sm transition">
+                  {user?.email}
+                </span>
+                <button
+                  onClick={logout}
+                  className="text-sm text-red-600 hover:underline"
+                >
+                  Logout
+                </button>
+              </>
+            )}
           </li>
         </ul>
       </div>
