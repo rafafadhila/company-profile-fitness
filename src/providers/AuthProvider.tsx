486f148c74065ff3b206c2290293f3fc57fb7aaa
rafafadhila <rafafadhila03@gmail.com>
2025-11-03 15:34:28 +0700

    add protected page for /auth

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,42 +1,45 @@
 'use client'
 
-import { axiosInstance } from "@/utils/axios-instance"
-import { toast } from "react-toastify"
-import useAuthStore from "@/stores/useAuthStore"
-import { useEffect } from "react"
+import { axiosInstance } from "@/utils/axios-instance";
+import { toast } from "react-toastify";
+import useAuthStore from "@/stores/useAuthStore";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
-export default function AuthProvider({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function AuthProvider({ children }: { children: React.ReactNode }) {
     const { user, setUser } = useAuthStore();
+    const router = useRouter();
+    const pathname = usePathname();
 
     const onSessionLogin = async () => {
         try {
-
-            const response = await axiosInstance.get('/auth/session-login', {
+            const response = await axiosInstance.get("/auth/session-login", {
                 headers: {
                     Authorization: `Bearer ${user?.objectId}`,
                 },
             });
-
-            setUser({
-                email: response?.data?.data?.email,
-                name: response?.data?.data?.name,
-                objectId: response?.data?.data?.objectId,
-
-            })
-
+            setUser(response.data.data);
         } catch (error: any) {
             toast.error(error?.message);
         }
     };
 
     useEffect(() => {
-        if (user?.objectId) onSessionLogin();
-    }, [user?.objectId])
+        const handleAuth = async () => {
+            if (user?.objectId) {
+                await onSessionLogin();
+                if (pathname.startsWith("/auth")) {
+                    router.push("/");
+                }
+            } else {
+                const isProtected = !pathname.startsWith("/auth") && pathname !== "/";
+                if (isProtected) {
+                    router.push("/auth/login");
+                }
+            }
+        };
+        handleAuth();
+    }, [user?.objectId, pathname]);
 
-    return <>{ children } </>
-
+    return <>{children}</>;
 }
