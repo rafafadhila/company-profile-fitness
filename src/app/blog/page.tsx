d4de1ec4ea45c6b231fbc1d625b91d3f094a0ae9
rafafadhila <rafafadhila03@gmail.com>
2025-11-04 19:20:39 +0700

    fix blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,26 @@
-import Header from "../components/Header";
-import BlogList from "../components/blog/BlogList";
+import BlogList from "../components/blog/BlogList"
+import Header from "../components/Header"
 
-export default function page() {
+
+async function getBlogs() {
+    const response = await fetch(`${process.env.API_BASE_URL}/blogs`, {
+        cache: 'no-store',
+    })
+    const data = await response.json()
+    return data?.data || []
+}
+
+export default async function BlogPage() {
+    const blogs = await getBlogs()
+
     return (
-        <main className="bg-white">
+        <div className="min-h-screen bg-gray-50">
             <Header
                 image="/images/header.jpg"
                 title="Blog"
                 subtitle="Learning is the best for our body.">
             </Header>
-            <BlogList></BlogList>
-        </main>
+            <BlogList blogs={blogs} />
+        </div>
     )
 }
