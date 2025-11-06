50d9bc20699992cde1bf9a46a1d519ff1ab047f9
rafafadhila <rafafadhila03@gmail.com>
2025-11-06 11:29:47 +0700

    base url deploy

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://company-profile-fitness.vercel.app//api"
 })
 
 // Perlu diganti URL nya pas deploy
